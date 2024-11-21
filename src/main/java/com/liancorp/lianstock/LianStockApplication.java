fc611a9cdfb2d99a8ceba19ed36af13e4db64a1a
3lPr1mo <erickenriquechaparro@gmail.com>
2024-11-20 19:30:00 -0500

    project init

---
+++ src/main/java/com/liancorp/lianstock/LianStockApplication.java
@@ -0,0 +1,13 @@
+package com.liancorp.lianstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LianStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LianStockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liancorp/lianstock/LianStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liancorp.lianstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LianStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
