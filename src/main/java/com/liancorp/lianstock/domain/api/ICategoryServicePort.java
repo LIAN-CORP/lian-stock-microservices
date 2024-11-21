40e3a4d2f2f2fc91df7a0a7b04e17008bfb68b88
3lPr1mo <erickenriquechaparro@gmail.com>
2024-11-20 22:04:34 -0500

    refactor: Category service port now return Mono<Category>

--- src/main/java/com/liancorp/lianstock/domain/api/ICategoryServicePort.java
+++ src/main/java/com/liancorp/lianstock/domain/api/ICategoryServicePort.java
@@ -1,9 +1,8 @@
 package com.liancorp.lianstock.domain.api;
 
 import com.liancorp.lianstock.domain.model.Category;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface ICategoryServicePort {
-    Mono<Void> saveCategory(Category category);
+    Mono<Category> saveCategory(Category category);
 }
