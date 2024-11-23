21d90ae548e51bd6c9352c80fecf9e887a827828
3lPr1mo <erickenriquechaparro@gmail.com>
2024-11-23 17:29:44 -0500

    refactor: unused handler exception

--- src/main/java/com/liancorp/lianstock/infrastructure/driving/http/exceptionhandler/ControllerExceptionHandler.java
+++ src/main/java/com/liancorp/lianstock/infrastructure/driving/http/exceptionhandler/ControllerExceptionHandler.java
@@ -2,24 +2,13 @@ package com.liancorp.lianstock.infrastructure.driving.http.exceptionhandler;
 
 import com.liancorp.lianstock.domain.exception.CategoryAlreadyExistsException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach(error -> errors.put(error.getDefaultMessage(), error.getDefaultMessage()));
-        return errors;
-    }
 
     @ExceptionHandler(CategoryAlreadyExistsException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
