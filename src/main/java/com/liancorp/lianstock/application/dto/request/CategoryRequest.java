884120ba0889c29815120c4d3367a34644bb3a89
3lPr1mo <erickenriquechaparro@gmail.com>
2024-11-23 09:30:38 -0500

    refactor: change @Max to @Size to validate strings length

--- src/main/java/com/liancorp/lianstock/application/dto/request/CategoryRequest.java
+++ src/main/java/com/liancorp/lianstock/application/dto/request/CategoryRequest.java
@@ -1,18 +1,18 @@
 package com.liancorp.lianstock.application.dto.request;
 
 import com.liancorp.lianstock.application.constants.RequestConstants;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record CategoryRequest(
         @NotNull(message = RequestConstants.CATEGORY_NAME_MUST_NOT_BE_NULL)
-        @Max(value = RequestConstants.CATEGORY_NAME_MAX_LENGTH, message = RequestConstants.CATEGORY_NAME_IS_ABOVE_THE_LIMIT)
+        @Size(max = RequestConstants.CATEGORY_NAME_MAX_LENGTH, message = RequestConstants.CATEGORY_NAME_IS_ABOVE_THE_LIMIT)
         @Pattern(regexp = RequestConstants.ONLY_LETTERS_REGEX)
         String name,
 
         @NotNull(message = RequestConstants.CATEGORY_DESCRIPTION_MUST_NOT_BE_NULL)
-        @Max(value = RequestConstants.CATEGORY_DESCRIPTION_MAX_LENGTH, message = RequestConstants.CATEGORY_DESCRIPTION_IS_ABOVE_THE_LIMIT)
+        @Size(max = RequestConstants.CATEGORY_DESCRIPTION_MAX_LENGTH, message = RequestConstants.CATEGORY_DESCRIPTION_IS_ABOVE_THE_LIMIT)
         @Pattern(regexp = RequestConstants.ONLY_LETTERS_REGEX)
         String description
 ) {
