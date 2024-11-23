68f8c95a1d91859068abf4fbb28a1a378a167f02
3lPr1mo <erickenriquechaparro@gmail.com>
2024-11-23 09:29:36 -0500

    refactor: number constants now with int type

--- src/main/java/com/liancorp/lianstock/application/constants/RequestConstants.java
+++ src/main/java/com/liancorp/lianstock/application/constants/RequestConstants.java
@@ -3,9 +3,9 @@ package com.liancorp.lianstock.application.constants;
 public class RequestConstants {
     private RequestConstants() {}
 
-    public static final Integer CATEGORY_NAME_MAX_LENGTH = 50;
-    public static final Integer CATEGORY_DESCRIPTION_MAX_LENGTH = 150;
-    public static final String ONLY_LETTERS_REGEX = "^[a-zA-Z]*$";
+    public static final int CATEGORY_NAME_MAX_LENGTH = 50;
+    public static final int CATEGORY_DESCRIPTION_MAX_LENGTH = 150;
+    public static final String ONLY_LETTERS_REGEX = "^[\\p{L}\\s\\D]+$";
     public static final String CATEGORY_NAME_MUST_NOT_BE_NULL = "Category name must not be null";
     public static final String CATEGORY_DESCRIPTION_MUST_NOT_BE_NULL = "Category description must not be null";
     public static final String CATEGORY_NAME_IS_ABOVE_THE_LIMIT = "Category name is above the limit";
