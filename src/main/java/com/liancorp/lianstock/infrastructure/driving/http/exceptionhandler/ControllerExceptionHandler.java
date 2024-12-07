200f7bfa2bd1730f13d0e310c80843a12c1d2cd9
3lPr1mo <erickenriquechaparro@gmail.com>
2024-12-07 15:19:28 -0500

    refactor(infra): add functions to handle subcategory domain exceptions

--- src/main/java/com/liancorp/lianstock/infrastructure/driving/http/exceptionhandler/ControllerExceptionHandler.java
+++ src/main/java/com/liancorp/lianstock/infrastructure/driving/http/exceptionhandler/ControllerExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.liancorp.lianstock.infrastructure.driving.http.exceptionhandler;
 
 import com.liancorp.lianstock.domain.exception.CategoryAlreadyExistsException;
+import com.liancorp.lianstock.domain.exception.CategoryIdDoNotExists;
+import com.liancorp.lianstock.domain.exception.SubcategoryAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -20,6 +22,18 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(WebExchangeBindException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleWebExchangeBindException(WebExchangeBindException ex) {
+        return new ErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+    }
+
+    @ExceptionHandler(SubcategoryAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleSubcategoryAlreadyExistsException(SubcategoryAlreadyExistsException ex) {
+        return new ErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+    }
+
+    @ExceptionHandler(CategoryIdDoNotExists.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleCategoryIdDoNotExists(CategoryIdDoNotExists ex) {
         return new ErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), HttpStatus.BAD_REQUEST.value());
     }
 
