8dddff650f8fa02659960c00d535b8ee8163cec6
3lPr1mo <erickenriquechaparro@gmail.com>
2024-12-06 19:40:49 -0500

    refactor: add subcategory exists error constant

--- src/main/java/com/liancorp/lianstock/domain/constants/ErrorConstants.java
+++ src/main/java/com/liancorp/lianstock/domain/constants/ErrorConstants.java
@@ -4,4 +4,5 @@ public class ErrorConstants {
     private ErrorConstants() {}
 
     public static final String CATEGORY_ALREADY_EXISTS = "Category already exists: ";
+    public static final String SUBCATEGORY_ALREADY_EXISTS = "Subcategory already exists: ";
 }
