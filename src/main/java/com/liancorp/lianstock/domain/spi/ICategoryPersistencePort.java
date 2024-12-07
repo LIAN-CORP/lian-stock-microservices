9de33d410534385563c42ddb92a7d3e4587bcdcb
3lPr1mo <erickenriquechaparro@gmail.com>
2024-12-07 15:14:09 -0500

    refactor: add methods to find if exists category by UUID

--- src/main/java/com/liancorp/lianstock/domain/spi/ICategoryPersistencePort.java
+++ src/main/java/com/liancorp/lianstock/domain/spi/ICategoryPersistencePort.java
@@ -4,8 +4,11 @@ import com.liancorp.lianstock.domain.model.Category;
 import com.liancorp.lianstock.domain.model.ContentPage;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
 public interface ICategoryPersistencePort {
     Mono<Category> saveCategory(Category category);
     Mono<Boolean> categoryExistsByName(String name);
     Mono<ContentPage<Category>> findAllCategories(int page, int size, String sort, boolean isAsc);
+    Mono<Boolean> categoryExistsById(UUID id);
 }
