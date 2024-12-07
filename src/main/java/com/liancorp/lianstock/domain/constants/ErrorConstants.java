50127648bfce48765427a171f99db051e9e65c5e
3lPr1mo <erickenriquechaparro@gmail.com>
2024-12-07 15:13:09 -0500

    refactor: add category UUID do not exists constant

--- src/main/java/com/liancorp/lianstock/domain/constants/ErrorConstants.java
+++ src/main/java/com/liancorp/lianstock/domain/constants/ErrorConstants.java
@@ -5,4 +5,5 @@ public class ErrorConstants {
 
     public static final String CATEGORY_ALREADY_EXISTS = "Category already exists: ";
     public static final String SUBCATEGORY_ALREADY_EXISTS = "Subcategory already exists: ";
+    public static final String CATEGORY_NOT_EXISTS_WITH_UUID = "Category not exists with UUID: ";
 }
