c6fca7adfa796760aaaf67134a0bac9231b1ea8a
3lPr1mo <erickenriquechaparro@gmail.com>
2024-12-08 14:33:17 -0500

    refactor(infra): WebExchangeBind exception commented for the future

--- src/main/java/com/liancorp/lianstock/infrastructure/driving/http/exceptionhandler/ControllerExceptionHandler.java
+++ src/main/java/com/liancorp/lianstock/infrastructure/driving/http/exceptionhandler/ControllerExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.bind.support.WebExchangeBindException;
 
 
 @RestControllerAdvice
@@ -19,11 +18,11 @@ public class ControllerExceptionHandler {
         return new ErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), HttpStatus.BAD_REQUEST.value());
     }
 
-    @ExceptionHandler(WebExchangeBindException.class)
+    /*@ExceptionHandler(WebExchangeBindException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleWebExchangeBindException(WebExchangeBindException ex) {
         return new ErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), HttpStatus.BAD_REQUEST.value());
-    }
+    }*/
 
     @ExceptionHandler(SubcategoryAlreadyExistsException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
